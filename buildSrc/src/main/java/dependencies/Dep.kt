be69b5cc3095ed23a4affb450da9f7212df0e3ca
Atsumi3 <atsumi@mail.bizen.jp>
2021-06-30 08:17:30 +0000

    Upgraded org.jetbrains.kotlin:kotlin-gradle-plugin from 1.5.10 to 1.5.20

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -3,7 +3,7 @@ package dependencies
 class Dep {
     object GradlePlugin {
         const val android = "com.android.tools.build:gradle:4.2.1"
-        const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10"
+        const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20"
         const val releaseHub = "com.dipien:releases-hub-gradle-plugin:2.0.2"
     }
 
